0fcafe61438f58048ab54ea92452d989ae8b3b3b
Yura Klyosov <klyosovyura@gmail.com>
2022-11-03 16:53:53 +0400

    registration version 3

--- examp.js
+++ examp.js
@@ -286,3 +286,15 @@
 //       $("#div1").html(txt);
 //     });
 // });
+
+
+//  for registration v3
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
